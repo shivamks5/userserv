d68af4f3632bd47fe213e6251fcab402a4a74d7d
Shivam Singh <shivamk.singh2001@gmail.com>
2025-07-13 20:01:42 +0530

    add prometheus metrics middleware in service

--- cmd/main.go
+++ cmd/main.go
@@ -8,6 +8,7 @@ import (
 
 	"github.com/go-kit/log"
 	"github.com/shivamks5/userserv/endpoint"
+	"github.com/shivamks5/userserv/metrics"
 	"github.com/shivamks5/userserv/middleware"
 	"github.com/shivamks5/userserv/service"
 	"github.com/shivamks5/userserv/transport"
@@ -18,7 +19,10 @@ func main() {
 	logger = log.NewLogfmtLogger(os.Stdout)
 	logger = log.With(logger, "ts", log.DefaultTimestamp, "caller", log.DefaultCaller)
 
+	mt := metrics.NewPrometheusMetrics("user_service")
+
 	svc := service.NewUserService()
+	svc = middleware.NewMetricsMiddleware(svc, mt)
 	svc = middleware.NewLoggingMiddleware(logger, svc)
 
 	eps := endpoint.NewEndpoints(svc)
